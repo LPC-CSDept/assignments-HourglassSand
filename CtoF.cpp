8b6f10b697eaa3182c0fde92e074d0c061d9a377
HourglassSand <salechuga@zonemail.clpccd.edu>
2022-02-10 11:54:22 -0800

    CtoF.cpp

---
+++ CtoF.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float C;
+	float F;
+
+	cout << "Enter your celsius degree ";
+	cin >> C;
+
+	F = 9 / 5.0 * C + 32;
+	cout << "The Farenheit : " << F << endl;
+
+}
