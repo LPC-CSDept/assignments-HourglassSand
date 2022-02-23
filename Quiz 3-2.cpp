89d86193aba31aaf78b1f71846808e2b8252bc33
HourglassSand <salechuga@zonemail.clpccd.edu>
2022-02-22 16:10:05 -0800

    Quiz 3-2.cpp

---
+++ Quiz 3-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>  // C standard Library
+#include <ctime> 
+#include <iomanip> 
+using namespace std;
+
+int main()
+{
+	int 6, 8, 10;
+	srand(time(0));
+	
+	6 = rand() % 100 ; // 0 ~ 99  
+	cout << num1 << endl;
+
+	8 = rand() % 100 ; // 0 ~ 99  
+	cout << num2 << endl;
+
+    10 = rand() % 100 ; // 0 ~ 99  
+	cout << num2 << endl;
+
+
+    sum = 6 + 8 + 10;
+	avg = sum / 3.0;
+}
